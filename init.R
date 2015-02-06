51fd31fc3cbc24db8236dc77baf16be4bb92a7fb
Taffarel de Lima <taffarel.lima@dafiti.com.br>
2015-02-06 11:31:09 -0200

    Third submission, all woman from third class has more loss than the general woman

--- init.R
+++ init.R
@@ -24,3 +24,24 @@ test$Survived[test$Sex == 'female'] <- 1
 str(test)
 submit2 <- data.frame(PassengerId = test$PassengerId, Survived = test$Survived)
 write.csv(submit2, file = "allmenpersih.csv", row.names = FALSE)
+
+summary(train$Age)
+train$Child <- 0
+train$Child[train$Age < 18] <- 1
+
+aggregate(Survived ~ Child + Sex, data=train, FUN=sum)
+aggregate(Survived ~ Child + Sex, data=train, FUN=length)
+aggregate(Survived ~ Child + Sex, data=train, FUN=function(x){sum(x)/length(x)})
+
+train$Fare2 <- '30+'
+train$Fare2[train$Fare < 30 & train$Fare >= 20] <- '20-30'
+train$Fate2[train$Fare < 20 & train$Fare >= 10] <- '10-20'
+train$Fare2[train$Fare < 10] <- '<10'
+
+aggregate(Survived ~ Fare2 + Pclass + Sex, data=train, FUN=function(x) {sum(x)/length(x)})
+test$Survived <- 0
+test$Survived[test$Sex == 'female'] <- 1
+test$Survived[test$Sex == 'female' & test$Pclass == 3 & test$Fare >= 20] <- 0
+
+submit3 <- data.frame(PassengerId = test$PassengerId, Survived = test$Survived)
+write.csv(submit3, file = 'allwomaninthirdclas.csv', row.names = FALSE)
